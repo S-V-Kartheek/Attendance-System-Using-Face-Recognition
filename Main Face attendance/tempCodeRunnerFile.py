9c851a3e574ba2b34464312b1a9e40d72ca085a7
Venkata Kartheek Sanka <venkatakartheeksanka@gmail.com>
2025-04-05 06:19:02 +0530

    Delete Main Face attendance directory

--- Main Face attendance/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Error: No webcam found. Trying next index.")
-    cap = cv2.VideoCapture(1)  # Try next device
-    if not cap.isOpened():
-        raise Exception("No webcam available.")
